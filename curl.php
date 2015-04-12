15b5677f98684110ee8c59cb14fe6d091abd3540
git <admin@lianyue.org>
2015-04-12 20:45:47 +0900

    `

--- curl.php
+++ curl.php
@@ -8,7 +8,7 @@
 /*	Author: Moon
 /*
 /*	Created: UTC 2015-03-25 09:14:34
-/*	Updated: UTC 2015-04-02 13:51:04
+/*	Updated: UTC 2015-04-09 02:13:32
 /*
 /* ************************************************************************** */
 namespace Loli;
@@ -17,9 +17,3 @@ namespace Loli;
 
 require __DIR__ . '/config.php';
 require __DIR__ . '/vendor/autoload.php';
-
-
-$CURL = new CURL();
-
-$CURL->add('', ['url' => 'http://www.loli.dev/?x=1', CURLOPT_POSTFIELDS => "['qq' => 'qq']"]);
-echo $CURL->get();
